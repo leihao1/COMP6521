ce8eee04b63ea5437fa2db83e21988fe665cc2ce
Lei Hao <leihao1313@gmail.com>
2019-04-08 11:29:43 -0400

    update

--- lab2/src/CONSTANT.java
+++ lab2/src/CONSTANT.java
@@ -1,4 +1,4 @@
 public class CONSTANT {
     public static final int BUCKET_SIZE = 20;
-    public static final int BUCKET_NUM = 1000/BUCKET_SIZE +1;
+    public static final int BUCKET_NUM = 1000/BUCKET_SIZE;
 }
