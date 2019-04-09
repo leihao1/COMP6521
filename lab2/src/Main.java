5efa5b7555b2e3c2c38a7d4484982bb05036a86b
Lei Hao <leihao1313@gmail.com>
2019-04-08 21:02:40 -0400

    use StringBuilder to build index

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -7,7 +7,8 @@ public class Main {
     private static  boolean inDisk = false;
     private static String fileName = "input\\LA2.txt";
     private static String filePath = System.getProperty("user.dir")+"\\lab2\\";
-    private static HashMap<Integer,HashMap<Integer,HashMap<Integer,ArrayList<String>>>> index = new HashMap<>();
+//    private static HashMap<Integer,HashMap<Integer,HashMap<Integer,ArrayList<String>>>> index = new HashMap<>();
+    private static HashMap<Integer,HashMap<Integer,HashMap<Integer,StringBuilder>>> index = new HashMap<>();
     private static ArrayList<Float> testx = new ArrayList<>();
     private static ArrayList<Float> testy = new ArrayList<>();
     private static ArrayList<Float> testz = new ArrayList<>();
@@ -17,7 +18,6 @@ public class Main {
         System.out.println("Free mem "+Runtime.getRuntime().freeMemory()/1000000);
 
         buildIndex();
-
 //        System.out.println("Memory used for index : "+MemoryUtil.deepMemoryUsageOf(index)/1000000.0);
         while (true){
             System.out.println(" ");
@@ -37,6 +37,7 @@ public class Main {
                     String[][] rq = new String[3][2];
                     while(true) {
                         System.out.println(" ");
+                        System.out.println("Range Query...");
                         System.out.println("Please enter lower and upper bounds for x y z (separate by space) :");
                         System.out.println("[Press \"Enter\" To Return Main Menu]");
                         String[] query;
@@ -70,6 +71,7 @@ public class Main {
                     String[] nnq = new String[3];
                     while (true){
                         System.out.println(" ");
+                        System.out.println("K-NN Query...");
                         System.out.println("Enter Target Point x y z Coordinates (separate by space) :");
                         System.out.println("[Press \"Enter\" To Return Main Menu]");
                         String[] query;
@@ -100,7 +102,7 @@ public class Main {
 
         }
     }
-
+/*
     private static void buildIndex(){
         System.out.println("Start Building Index ...");
         long start  = System.currentTimeMillis();
@@ -204,6 +206,140 @@ public class Main {
         boolean wholeFile = false;
         if(XL==0 && XH==1000 && YL==0 && YH==1000 && ZL==0 && ZH==1000){
             wholeFile = true;
+    //            System.out.println("whole ");
+        }
+
+        for (int i = xLow; i <= xHigh; i++) {
+            if (index.containsKey(i)) {
+                for (int j = yLow; j <= yHigh; j++) {
+                    if (index.get(i).containsKey(j)) {
+                        for (int k = zLow; k <= zHigh; k++) {
+                            if (index.get(i).get(j).containsKey(k)) {
+                                result.addAll(index.get(i).get(j).get(k));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        ArrayList<String> newResult = new ArrayList<>();
+        //check range
+        if(!wholeFile) {
+            for (String line : result) {
+                double x = Double.parseDouble(line.substring(1, 9));
+                double y = Double.parseDouble(line.substring(11, 19));
+                double z = Double.parseDouble(line.substring(21, 29));
+                if (x >= XL && x <= XH && y >= YL && y <= YH && z >= ZL && z <= ZH) {
+                    newResult.add(line);
+                }
+            }
+        }else{
+            newResult.addAll(result);
+        }
+        System.out.println("Range Query Finished. Time: "+(System.currentTimeMillis()-start)/1000.0+" (s)");
+        System.out.println("Range Query Point Number : "+newResult.size());
+        return newResult;
+
+}
+*/
+    private static void buildIndex(){
+        System.out.println("Start Building Index ...");
+        long start  = System.currentTimeMillis();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath+fileName));
+//            RandomAccessFile ra = new RandomAccessFile(filePath+fileName,"r");
+            String line = null;
+            int pointer = 0;
+            try {
+                line = br.readLine();
+//                pointer = ra.getFilePointer();
+//                line = ra.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int i =0;
+            int j = 0;
+            while(line !=null){
+//                if(i%1000000 == 0){
+//                    System.out.println("Finish tuples: "+i);
+//                }
+
+                //twice faster than using split()
+                String x = line.substring(1,9);
+                String y = line.substring(11,19);
+                String z = line.substring(21,29);
+                int xKey = (int)Math.ceil(Double.valueOf(x)/CONSTANT.BUCKET_SIZE);
+                int yKey = (int)Math.ceil(Double.valueOf(y)/CONSTANT.BUCKET_SIZE);
+                int zKey = (int)Math.ceil(Double.valueOf(z)/CONSTANT.BUCKET_SIZE);
+
+                if(!inDisk) {
+                    if (!index.containsKey(xKey)) {
+                        HashMap<Integer, HashMap<Integer, StringBuilder>> hy = new HashMap<>();
+                        HashMap<Integer, StringBuilder> hz = new HashMap<>();
+                        j++;
+                        hz.put(zKey, new StringBuilder("("+x+","+y+","+z+")"));
+                        hy.put(yKey, hz);
+                        index.put(xKey, hy);
+                    } else {
+                        if (!index.get(xKey).containsKey(yKey)) {
+                            HashMap<Integer, StringBuilder> hz = new HashMap<>();
+                            j++;
+                            hz.put(zKey, new StringBuilder("("+x+","+y+","+z+")"));
+                            index.get(xKey).put(yKey, hz);
+                        } else {
+                            if (!index.get(xKey).get(yKey).containsKey(zKey)) {
+                                j++;
+                                index.get(xKey).get(yKey).put(zKey, new StringBuilder("("+x+","+y+","+z+")"));
+                            } else {
+                                index.get(xKey).get(yKey).get(zKey).append("("+x+","+y+","+z+")");
+                                j++;
+                            }
+                        }
+                    }
+                }
+
+                try {
+                    line = br.readLine();
+//                    pointer = ra.getFilePointer();
+//                    line = ra.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                i++;
+                pointer += 31;
+            }//last tuple
+            System.out.println("Index build finished. Time : "+(System.currentTimeMillis()-start)/1000.0+" (s)");
+            System.out.println("Scan tuples i :" +i);
+            System.out.println("Add records j :"+j);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<String> rangeQuery(String[][] query){
+        assert (query.length ==3 && query[0].length == 2 ) : "Wrong query format!";
+        System.out.println("Start Range Query ...");
+        long start = System.currentTimeMillis();
+
+        ArrayList<StringBuilder> result = new ArrayList<>();
+        double XL = Double.valueOf(query[0][0]);
+        double XH = Double.valueOf(query[0][1]);
+        double YL = Double.valueOf(query[1][0]);
+        double YH = Double.valueOf(query[1][1]);
+        double ZL = Double.valueOf(query[2][0]);
+        double ZH = Double.valueOf(query[2][1]);
+        int xLow, xHigh, yLow, yHigh, zLow, zHigh;
+        xLow = (int) Math.ceil(XL / CONSTANT.BUCKET_SIZE);
+        xHigh = (int) Math.ceil(XH / CONSTANT.BUCKET_SIZE);
+        yLow = (int) Math.ceil(YL / CONSTANT.BUCKET_SIZE);
+        yHigh = (int) Math.ceil(YH / CONSTANT.BUCKET_SIZE);
+        zLow = (int) Math.ceil(ZL / CONSTANT.BUCKET_SIZE);
+        zHigh = (int) Math.ceil(ZH / CONSTANT.BUCKET_SIZE);
+
+        boolean wholeFile = false;
+        if(XL==0 && XH==1000 && YL==0 && YH==1000 && ZL==0 && ZH==1000){
+            wholeFile = true;
 //            System.out.println("whole ");
         }
 
@@ -213,27 +349,38 @@ public class Main {
                     if (index.get(i).containsKey(j)) {
                         for (int k = zLow; k <= zHigh; k++) {
                             if (index.get(i).get(j).containsKey(k)) {
-                                if (wholeFile)
-                                    result.addAll(index.get(i).get(j).get(k));
-                                else {
-                                    for (String line : index.get(i).get(j).get(k)) {
-                                        double x = Double.valueOf(line.substring(1, 9));
-                                        double y = Double.valueOf(line.substring(11, 19));
-                                        double z = Double.valueOf(line.substring(21, 29));
-                                        if (x >= XL && x <= XH && y >= YL && y <= YH && z >= ZL && z <= ZH) {
-                                            result.add(line);
-                                        }
-                                    }
-                                }
+                                result.add(index.get(i).get(j).get(k));
                             }
                         }
                     }
                 }
             }
         }
+
+        ArrayList<String> newResult = new ArrayList<>();
+        //check range
+        if(!wholeFile) {
+            for (StringBuilder sb : result) {
+                int pointer =0;
+                while(pointer<= sb.length()-28){
+                    double x = Double.parseDouble(sb.toString().substring(pointer+1, pointer+9));
+                    double y = Double.parseDouble(sb.toString().substring(pointer+10, pointer+18));
+                    double z = Double.parseDouble(sb.toString().substring(pointer+19, pointer+27));
+                    pointer += 28;
+                    if (x >= XL && x <= XH && y >= YL && y <= YH && z >= ZL && z <= ZH) {
+                        newResult.add("("+x+","+y+","+z+")");
+                    }
+
+                }
+            }
+        }else{
+            for(StringBuilder sb :result){
+                newResult.add(sb.toString());
+            }
+        }
         System.out.println("Range Query Finished. Time: "+(System.currentTimeMillis()-start)/1000.0+" (s)");
-        System.out.println("Range Query Point Number : "+result.size());
-        return result;
+        System.out.println("Range Query Point Number : "+newResult.size());
+        return newResult;
 
     }
 
@@ -459,7 +606,7 @@ public class Main {
         String outFileName  = name.toString().substring(0,name.length()-1);
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter(filePath+"output\\"
-                    +outFileName+suffix,true));
+                    +outFileName+suffix,false));
             for(String s:result){
                 bw.write(s+"\n");
             }
@@ -467,7 +614,7 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Result Saved at file : "+filePath+"output\\"+outFileName+suffix);
+        System.out.println("Result File Saved At : "+filePath+"output\\"+outFileName+suffix);
     }
 
     private static ArrayList<String> retrieveDataFromDisk(ArrayList<String> pointers){
@@ -492,88 +639,33 @@ public class Main {
         return result;
     }
 
-    private static void writeIndexToDisk(){
-        int j = 0;
-        for(Map.Entry<Integer,HashMap<Integer,HashMap<Integer,ArrayList<String>>>> entry:index.entrySet()){
-            for(Map.Entry<Integer,HashMap<Integer,ArrayList<String>>> entry1 : entry.getValue().entrySet()){
-                for(Map.Entry<Integer,ArrayList<String>> entry2:entry1.getValue().entrySet()){
-                    try
-                    {
-                        BufferedWriter writer;
-                        File file = new File(filePath+"output\\index.index");
-                        file.createNewFile();
+//    private static void writeIndexToDisk(){
+//        int j = 0;
+//        for(Map.Entry<Integer,HashMap<Integer,HashMap<Integer,ArrayList<String>>>> entry:index.entrySet()){
+//            for(Map.Entry<Integer,HashMap<Integer,ArrayList<String>>> entry1 : entry.getValue().entrySet()){
+//                for(Map.Entry<Integer,ArrayList<String>> entry2:entry1.getValue().entrySet()){
+//                    try
+//                    {
+//                        BufferedWriter writer;
+//                        File file = new File(filePath+"output\\index.index");
+//                        file.createNewFile();
+//
+//                        writer = new BufferedWriter(new FileWriter(file));
+//                        for(String s : entry2.getValue()){
+//                            writer.write(s);
+//                            j++;
+//                            if(j%1000000 ==0)
+//                                System.out.println("j :"+j);
+//                        }
+//                    }catch(IOException ioe)
+//                    {
+//                        ioe.printStackTrace();
+//                    }
+//                }
+//            }
+//        }
+//
+//        System.out.println("write fiish");
+//    }
 
-                        writer = new BufferedWriter(new FileWriter(file));
-                        for(String s : entry2.getValue()){
-                            writer.write(s);
-                            j++;
-                            if(j%1000000 ==0)
-                                System.out.println("j :"+j);
-                        }
-                    }catch(IOException ioe)
-                    {
-                        ioe.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        System.out.println("write fiish");
-    }
-
-    private static void test(){
-        System.out.println("Start test ...");
-        long start  = System.currentTimeMillis();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filePath+fileName));
-            String line = null;
-            try {
-                line = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            int i =0;
-            int j = 0;
-            while(line !=null){
-                if(i%1000000 == 0){
-                    System.out.println("Finish tuple: "+i);
-                }
-                String x = line.substring(1,9);
-                String y = line.substring(11,19);
-                String z = line.substring(21,29);
-
-                testx.add(Float.valueOf(x));
-                testy.add(Float.valueOf(y));
-                testz.add(Float.valueOf(z));
-
-                try {
-                    line = br.readLine();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                i++;
-            }//last tuple
-            System.out.println("Index build finished. Time : "+(System.currentTimeMillis()-start)/1000.0+" (s)");
-            System.out.println("Scan tuples i :" +i);
-            System.out.println("Add records j :"+j);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static ArrayList<String> testRangeQuery(String[][] rq) {
-        ArrayList<String> result = new ArrayList<>();
-        ArrayList<String> qx = new ArrayList<>();
-        ArrayList<String> qy = new ArrayList<>();
-        ArrayList<String> qz = new ArrayList<>();
-        float x1,x2,y1,y2,z1,z2;
-        x1 = Float.valueOf(rq[0][0]);
-        x2 = Float.valueOf(rq[0][1]);
-        y1 = Float.valueOf(rq[1][0]);
-        y2 = Float.valueOf(rq[1][1]);
-        z1 = Float.valueOf(rq[2][0]);
-        z1 = Float.valueOf(rq[2][1]);
-
-        return result;
-    }
 }
